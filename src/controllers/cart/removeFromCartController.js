7677c81e8f118177b298c5ae5d29c9530d71b073
Watsuphol <wasumc.design@gmail.com>
2024-07-09 15:24:16 +0700

    Updated before pull

--- src/controllers/cart/removeFromCartController.js
+++ src/controllers/cart/removeFromCartController.js
@@ -1,10 +1,11 @@
+import NotFoundError from '../../error/NotFoundError.js';
 import Cart from '../../models/cartModel.js';
 
 const removeFromCart = async (req, res, next) => {
   try {
     const cart = await Cart.findByIdAndDelete(req.params.id);
     if (!cart) {
-      return res.status(404).send();
+      throw new NotFoundError(`Cart can't remove`);
     }
     res.status(200).send(cart);
   } catch (error) {
