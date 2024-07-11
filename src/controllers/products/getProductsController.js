88fd68f96f57ad2eb5ced12f592ed85048827730
tanatut12 <tanatut12@gmail.com>
2024-07-11 11:39:51 +0700

    filter update

--- src/controllers/products/getProductsController.js
+++ src/controllers/products/getProductsController.js
@@ -1,13 +1,31 @@
-import Product from "../../models/productModel.js"
+import Product from "../../models/productModel.js";
 
+const getAllProducts = async (req, res) => {
+    try {
+        // Get the category from the query parameters
+        const category = req.query.category;
+        
+        let products;
 
-const getAllProducts = async (req,res)=>{
-    let products = await Product.find({})
-    console.log("All product fetched")
-    res.send(products)
-}
+       
+        if (category) {
+           
+            const validCategories = ["Feeder", "Toilets", "Towers", "Toys"];
+            if (!validCategories.includes(category)) {
+                return res.status(400).send({ error: "Invalid category" });
+            }
+            products = await Product.find({ category: category });
+        } else {
+            
+            products = await Product.find({});
+        }
 
+        console.log("Products fetched");
+        res.send(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Internal Server Error" });
+    }
+};
 
-
-
-export default getAllProducts
+export default getAllProducts;
